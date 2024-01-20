8ba78e36a01174909b3f19811fab04b52f1298bc
Scott Drinkwater <s.p.drinkwater@gmail.com>
2024-01-20 23:22:42 +0000

    Improve docs

--- src/schedule.ts
+++ src/schedule.ts
@@ -1,5 +1,6 @@
 import { addDays, addMonths, addWeeks, addYears, min } from "date-fns";
 
+// Unit of time to measure frequency an event occurs
 export const Unit = {
   day: "day",
   week: "week",
@@ -9,11 +10,13 @@ export const Unit = {
 
 export type UnitType = keyof typeof Unit;
 
+// Frequency of which an event occurs
 export type Frequency = {
   measure: number;
   unit: UnitType;
 };
 
+// The duration and frequency for an event
 export type Pattern<TEvent> = {
   event: TEvent;
   startDate: Date;
@@ -21,24 +24,40 @@ export type Pattern<TEvent> = {
   frequency: Frequency;
 };
 
+// An occurrence of an event for a single date on a schedule
 type Occurrence<TEvent> = {
   event: TEvent;
   date: Date;
 };
 
-const nextOccurrence = (period: Frequency, date: Date) => {
-  switch (period.unit) {
+/**
+ * Get the next occurrence of a date from a frequency.
+ * 
+ * @param {Frequency} frequency 
+ * @param {Date} date 
+ * @returns {Date}
+ */
+const nextOccurrence = (frequency: Frequency, date: Date) => {
+  switch (frequency.unit) {
   case "day":
-    return addDays(date, period.measure);
+    return addDays(date, frequency.measure);
   case "week":
-    return addWeeks(date, period.measure);
+    return addWeeks(date, frequency.measure);
   case "month":
-    return addMonths(date, period.measure);
+    return addMonths(date, frequency.measure);
   case "year":
-    return addYears(date, period.measure);
+    return addYears(date, frequency.measure);
   }
 };
 
+/**
+ * Get a list of concrete event occurrences from a pattern between a date range.
+ * 
+ * @param {Pattern<TEvent>} pattern 
+ * @param {Date} startDate 
+ * @param {Date} endDate 
+ * @returns {Occurrence<TEvent>}
+ */
 const occurrences = <TEvent>(
   pattern: Pattern<TEvent>,
   startDate: Date,
@@ -61,6 +80,7 @@ const occurrences = <TEvent>(
   return occurrences;
 };
 
+// The schedule containing all event patterns.
 type Schedule<TEvent> = {
   patterns: Pattern<TEvent>[];
 };
